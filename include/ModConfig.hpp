d342475057d08130d4092aab6476eb7dea3ae5d7
ArtemisE1Tara <ahmedsecen2@gmail.com>
2022-08-26 17:31:46 -0400

    first commit

---
+++ include/ModConfig.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "config-utils/shared/config-utils.hpp"
+
+
+DECLARE_CONFIG(ModConfig,
+    CONFIG_VALUE();
+
+    CONFIG_INIT_FUNCTION(
+        CONFIG_INIT_VALUE();
+
+    )
+)
